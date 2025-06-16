cc458afde747777e175ca42a5714237340b87d5b
Crisley Alves <crisleyalvesphx@gmail.com>
2025-06-16 11:49:09 -0300

    setup module alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@config': fileURLToPath(new URL('./src/config', import.meta.url)),
+      '@graphql': fileURLToPath(new URL('./src/graphql', import.meta.url)),
+      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+    }
+  }
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
+    alias: {
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@config": fileURLToPath(new URL("./src/config", import.meta.url)),
+      "@graphql": fileURLToPath(new URL("./src/graphql", import.meta.url)),
+      "@hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
+      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+      "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+      "@types": fileURLToPath(new URL("./src/types", import.meta.url)),
+      "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+    },
   },
 });
